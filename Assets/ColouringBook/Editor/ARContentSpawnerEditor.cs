694ef7413d87a278d05ba49b4ba2253cefb8a3f3
ThunderTrabuco <hhthunderbird@gmail.com>
2025-12-29 11:58:05 -0300

    refactor: clean up editor scripts and remove duplicates
    Merged FileModificationWarning.cs into ARContentSpawnerRefreshHelper.cs
    
    Removed redundant AssetPostprocessor and AssetModificationProcessor classes
    
    Updated ARContentSpawnerEditor to use new GUID-based system
    
    Fixed property name mismatches (imageName instead of name)

--- Assets/ColouringBook/Editor/ARContentSpawnerEditor.cs
+++ Assets/ColouringBook/Editor/ARContentSpawnerEditor.cs
@@ -1,114 +1,152 @@
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
+//using UnityEditor;
 
-namespace Felina.ARColoringBook.Editor
-{
-    [CustomEditor( typeof( ARContentSpawner ) )]
-    public class ARContentSpawnerEditor : UnityEditor.Editor
-    {
-        private ARContentSpawner _targetScript;
-        private SerializedProperty _contentLibraryProp; // the targetScript ARContentPair list to be filled
+//namespace Felina.ARColoringBook.Editor
+//{
+//    [CustomEditor( typeof( ARContentSpawner ) )]
+//    public class ARContentSpawnerEditor : UnityEditor.Editor
+//    {
+//        private ARContentSpawner _spawner;
+//        //private SerializedProperty _prefabPairsProp;
+//        //private ARTrackedImageManager _trackedImageManager;
+//        //private List<XRReferenceImage> _referenceImages = new();
 
-        void OnEnable()
-        {
-            _targetScript = ( ARContentSpawner ) target;
-            _contentLibraryProp = serializedObject.FindProperty( "_contentLibrary" ); 
-        }
+//        void OnEnable()
+//        {
+//            _spawner = target as ARContentSpawner;
+//            //_prefabPairsProp = serializedObject.FindProperty( "_prefabPairs" );
+//            //_trackedImageManager = _spawner.GetComponent<ARTrackedImageManager>();
 
-        public override void OnInspectorGUI()
-        {
-            serializedObject.Update();
+//            EditorApplication.hierarchyChanged += OnHierarchyChanged;
+//        }
 
-            DrawDefaultInspector();
+//        private void OnHierarchyChanged()
+//        {
+//            _spawner?.Reset();
+//        }
 
-            // resolve reference library: component > bridge component > scene manager > project first
-            XRReferenceImageLibrary chosenLib = null;
+//        public override void OnInspectorGUI()
+//        {
+//            //draw default inspector
+//            DrawDefaultInspector();
 
-            var bridge = _targetScript.GetComponent<IARBridge>();
-            ARTrackedImageManager imageManager = null;
-            if ( bridge != null ) imageManager = bridge.ARTrackedImageManager;
-            var sceneManager = imageManager != null ? imageManager : Object.FindObjectOfType<ARTrackedImageManager>();
-            var list = sceneManager != null ? sceneManager.referenceLibrary : null;
+//            //serializedObject.Update();
 
-            // go from here - use `list` (XRReferenceImageLibrary or IReferenceImageLibrary) provided above
-            string[] imageNames = new string[0];
-            if ( list != null )
-            {
-                int cnt = (int)list.count;
-                if ( cnt > 0 )
-                {
-                    imageNames = new string[ cnt ];
-                    for ( int i = 0; i < cnt; i++ ) imageNames[i] = list[ i ].name;
-                }
-            }
+//            //// Show info about ARTrackedImageManager
+//            //if ( _trackedImageManager == null )
+//            //{
+//            //    EditorGUILayout.HelpBox( "ARContentSpawner requires ARTrackedImageManager on the same GameObject!", MessageType.Error );
+//            //    if ( GUILayout.Button( "Add ARTrackedImageManager" ) )
+//            //    {
+//            //        _spawner.gameObject.AddComponent<ARTrackedImageManager>();
+//            //    }
+//            //    serializedObject.ApplyModifiedProperties();
+//            //    return;
+//            //}
 
-            EditorGUILayout.Space();
-            var arraySize = _contentLibraryProp != null ? _contentLibraryProp.arraySize : 0;
-            EditorGUILayout.LabelField( $"Mappings ({arraySize})", EditorStyles.boldLabel );
+//            //// Get reference library
+//            //var library = _trackedImageManager.referenceLibrary as XRReferenceImageLibrary;
+//            //if ( library == null )
+//            //{
+//            //    EditorGUILayout.HelpBox( "ARTrackedImageManager has no Reference Image Library assigned!", MessageType.Warning );
+//            //    serializedObject.ApplyModifiedProperties();
+//            //    return;
+//            //}
 
-            // 2. Draw list
-            if ( _contentLibraryProp == null )
-            {
-                EditorGUILayout.HelpBox( "Serialized property 'contentLibrary' not found on ARContentSpawner.", MessageType.Error );
-                serializedObject.ApplyModifiedProperties();
-                return;
-            }
+//            //// Check if library changed
+//            //if ( HasLibraryChanged( library ) )
+//            //{
+//            //    SyncPrefabPairsWithLibrary( library );
+//            //}
 
-            int listSize = _contentLibraryProp.arraySize;
-            for ( int i = 0; i < listSize; i++ )
-            {
-                var element = _contentLibraryProp.GetArrayElementAtIndex( i );
-                if ( element == null ) continue;
-                SerializedProperty prefabProp = element.FindPropertyRelative( "Prefab" );
-                SerializedProperty imageNameProp = element.FindPropertyRelative( "ImageName" );
+//            //// Draw prefab assignments
+//            //EditorGUILayout.Space();
+//            //EditorGUILayout.LabelField( $"Image Prefab Assignments ({library.count})", EditorStyles.boldLabel );
+//            //EditorGUILayout.Space();
 
-                if ( imageNameProp == null )
-                {
-                    EditorGUILayout.HelpBox( $"Element {i} missing 'ImageName' property.", MessageType.Warning );
-                    continue;
-                }
+//            //for ( int i = 0; i < library.count; i++ )
+//            //{
+//            //    var image = library[ i ];
+//            //    var prefab = _spawner.GetPrefabForImage( image );
 
-                EditorGUILayout.BeginVertical( EditorStyles.helpBox );
-                EditorGUILayout.BeginHorizontal();
-                GUILayout.Label( $"Entry {i + 1}", EditorStyles.miniLabel );
-                if ( GUILayout.Button( "Remove", EditorStyles.miniButtonRight, GUILayout.Width( 60 ) ) )
-                {
-                    _contentLibraryProp.DeleteArrayElementAtIndex( i );
-                    break;
-                }
-                EditorGUILayout.EndHorizontal();
+//            //    EditorGUILayout.BeginHorizontal( EditorStyles.helpBox );
 
-                // image dropdown
-                if ( imageNames.Length > 0 )
-                {
-                    int currentIndex = 0;
-                    for ( int j = 0; j < imageNames.Length; j++ ) if ( imageNames[j] == imageNameProp.stringValue ) { currentIndex = j; break; }
-                    int newIndex = EditorGUILayout.Popup( "Reference Image", currentIndex, imageNames );
-                    if ( newIndex >= 0 && newIndex < imageNames.Length ) imageNameProp.stringValue = imageNames[newIndex];
-                }
-                else
-                {
-                    EditorGUILayout.HelpBox( "No reference images available.", MessageType.Info );
-                }
+//            //    EditorGUILayout.LabelField( image.name, GUILayout.Width( 150 ) );
 
-                EditorGUILayout.PropertyField( prefabProp, new GUIContent( "Prefab to Spawn" ) );
-                EditorGUILayout.EndVertical();
-                EditorGUILayout.Space( 2 );
-            }
+//            //    var newPrefab = ( GameObject ) EditorGUILayout.ObjectField( prefab, typeof( GameObject ), false );
 
-            // 3. Add button
-            EditorGUILayout.Space();
-            if ( GUILayout.Button( "Add New Mapping" ) )
-            {
-                _contentLibraryProp.arraySize++;
-                var newElem = _contentLibraryProp.GetArrayElementAtIndex( _contentLibraryProp.arraySize - 1 );
-                newElem.FindPropertyRelative( "ImageName" ).stringValue = imageNames.Length > 0 ? imageNames[0] : string.Empty;
-                newElem.FindPropertyRelative( "Prefab" ).objectReferenceValue = null;
-            }
+//            //    if ( newPrefab != prefab )
+//            //    {
+//            //        Undo.RecordObject( target, "Change Prefab Assignment" );
+//            //        _spawner.SetPrefabForImage( image, newPrefab );
+//            //        EditorUtility.SetDirty( target );
+//            //    }
 
-            serializedObject.ApplyModifiedProperties();
-        }
-    }
-}
+//            //    EditorGUILayout.EndHorizontal();
+//            //}
+
+//            //serializedObject.ApplyModifiedProperties();
+//        }
+
+//        //private bool HasLibraryChanged( XRReferenceImageLibrary library )
+//        //{
+//        //    if ( library == null )
+//        //        return _referenceImages.Count > 0;
+
+//        //    if ( _referenceImages.Count != library.count )
+//        //        return true;
+
+//        //    for ( int i = 0; i < library.count; i++ )
+//        //    {
+//        //        if ( i >= _referenceImages.Count || _referenceImages[ i ].guid != library[ i ].guid )
+//        //            return true;
+//        //    }
+
+//        //    return false;
+//        //}
+
+//        //private void SyncPrefabPairsWithLibrary( XRReferenceImageLibrary library )
+//        //{
+//        //    // Update cached reference images
+//        //    _referenceImages.Clear();
+//        //    foreach ( var image in library )
+//        //    {
+//        //        _referenceImages.Add( image );
+//        //    }
+
+//        //    // Sync prefab pairs
+//        //    var existingPairs = new Dictionary<string, GameObject>();
+//        //    for ( int i = 0; i < _prefabPairsProp.arraySize; i++ )
+//        //    {
+//        //        var element = _prefabPairsProp.GetArrayElementAtIndex( i );
+//        //        var guidProp = element.FindPropertyRelative( "imageGuid" );
+//        //        var prefabProp = element.FindPropertyRelative( "prefab" );
+
+//        //        if ( guidProp != null && prefabProp != null )
+//        //        {
+//        //            existingPairs[ guidProp.stringValue ] = prefabProp.objectReferenceValue as GameObject;
+//        //        }
+//        //    }
+
+//        //    // Rebuild pairs list to match library
+//        //    _prefabPairsProp.ClearArray();
+//        //    foreach ( var image in library )
+//        //    {
+//        //        _prefabPairsProp.arraySize++;
+//        //        var element = _prefabPairsProp.GetArrayElementAtIndex( _prefabPairsProp.arraySize - 1 );
+
+//        //        element.FindPropertyRelative( "imageGuid" ).stringValue = image.guid.ToString();
+//        //        element.FindPropertyRelative( "imageName" ).stringValue = image.name;
+
+//        //        // Preserve existing prefab assignment if it exists
+//        //        if ( existingPairs.TryGetValue( image.guid.ToString(), out var existingPrefab ) )
+//        //        {
+//        //            element.FindPropertyRelative( "prefab" ).objectReferenceValue = existingPrefab;
+//        //        }
+//        //        else
+//        //        {
+//        //            element.FindPropertyRelative( "prefab" ).objectReferenceValue = null;
+//        //        }
+//        //    }
+//        //}
+//    }
+//}
